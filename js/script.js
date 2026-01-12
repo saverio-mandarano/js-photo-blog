9bc3885bc2582978e128c93a50072b73cbec9bea
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-12 15:01:16 +0100

    creo oggetto con url e id per carousel estrapolati con axios

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 
+// array globale per id e rispettiva immagine
+let carousel = []; 
+let j = 0;
+
 // creiamo la chiamata ajax all'endpoint
 axios.get(endpoint)
     .then(response => {
@@ -16,22 +20,28 @@ axios.get(endpoint)
         const posts = response.data;
         console.log(posts);
 
+        carousel = posts;
+
         // var di accumulo stringa output
         let postsOutput = "";
 
         // cicliamo l'array per estrapolare le info
         posts.forEach((post, index) => {
 
-            // destrutturiamo l'oggeto
-            const { title, date, url } = post;
+            // destrutturiamo l'oggetto
+            const { title, date, url, id } = post;
+
+            // salvo nell'array url e id per carousel
+            carousel[index] = { url: url, id: id };
 
             //console.log("il valore del titolo è :", title, "il valore del testo è :", date);
+            console.log("il valore dell'id è :", id);
 
             // valorizziamo la variabile di accumulo di output (stringa)
             postsOutput += `
             <div class="card p-3">
                 <div class="img-wrapper">
-                    <img src="${url}" class="card-img-top" />
+                    <img src="${url}" id="${id}" class="card-img-top" />
                     <img src="img/pin.svg" class="icon" />
                 </div>
                 <div class="card-body">
